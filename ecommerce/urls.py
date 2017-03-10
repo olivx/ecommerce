ed280dba839b2cb50b178309d8512df1c75fbed5
thiago oliveira <oliveiravicente.net@gmail.com>
2017-03-10 16:53:41 -0300

    templates produtos , produto , contato

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+    url(r'^produto/$', TemplateView.as_view(template_name='produto.html'), name='produto'),
     url(r'^produtos/$', TemplateView.as_view(template_name='produtos.html'), name='produtos'),
     url(r'^contato/$', TemplateView.as_view(template_name='contato.html'), name='contato'),
 
