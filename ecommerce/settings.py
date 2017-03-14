2c66790837ee522fd51c1ac5b8582779519b9689
thiago oliveira <oliveiravicente.net@gmail.com>
2017-03-14 14:18:36 -0300

    allowed_hosts to heroku

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('BEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[] , cast=Csv)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[] , cast=Csv())
 
 # Application definition
 
